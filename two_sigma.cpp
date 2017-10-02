54e0b32d7e00900931e64234e0c01693d2134ac0
Andersen <mizio.andersen@gmail.com>
2017-10-02 23:27:53 +0200

    Implemented reading of dataset /train/block0_values using pointers and a two dimensional array as output object. Checked that same results are reproduced with train_no_attrib.h5 which similar to train.h5 but without all the PyTables specific attributes.

--- two_sigma.cpp
+++ two_sigma.cpp
@@ -12,6 +12,9 @@
 using namespace H5;
 using namespace std;
 const H5std_string FILE_NAME( "train.h5" );
+// train_no_attrib.h5 is made using bash cmd
+// h5copy -v -i "train.h5" -o "train_no_attrib.h5" -s "/train" -d "/train" -f noattr -p
+// const H5std_string FILE_NAME( "train_no_attrib.h5" );
 const H5std_string dataset_axis0_name( "/train/axis0" );
 const H5std_string dataset_axis1_name( "/train/axis1" );
 const H5std_string dataset_block0_items_name( "/train/block0_items" );
@@ -23,7 +26,7 @@ const H5std_string dataset_name_select( "Matrix in file" );
 
 TwoSigmaFinModTools::TwoSigmaFinModTools(bool true_or_false = false, int n = 11)
 {
-    _input_file_name = "/pathToFile/train.h5";
+   _input_file_name = "/pathToFile/train.h5";
     _save_path = "/pathToDir/";
     is_portfolio_predictions = true_or_false;
     number_of_assets_in_portfolio = n;
@@ -47,22 +50,23 @@ TwoSigmaFinModTools::TwoSigmaFinModTools(bool true_or_false = false, int n = 11)
     // DataSet dataset = file.openDataSet( dataset_block0_items_name );
     // DataSet dataset = file.openDataSet( dataset_block0_values_name );
     // DataSet dataset = file_select.openDataSet( dataset_name_select );
-    DataSet * dataset = new DataSet (file->openDataSet( dataset_block0_items_name ));
+    // DataSet * dataset = new DataSet (file->openDataSet( dataset_block0_items_name ));
+    DataSet * dataset = new DataSet (file->openDataSet( dataset_block0_values_name ));
 
-    // Read an attribute using dynamic memory allocation on heap (new)
-    H5std_string test;
-    // Group * group = new Group (file->openGroup( "/" ));
-    Group * group = new Group (file->openGroup( "/train" ));
-    // Attribute * attr = new Attribute (group->openAttribute( "PYTABLES_FORMAT_VERSION" ));
+    // // Read an attribute using dynamic memory allocation on heap (new)
+    // H5std_string test;
+    // // Group * group = new Group (file->openGroup( "/" ));
+    // Group * group = new Group (file->openGroup( "/train" ));
+    // // Attribute * attr = new Attribute (group->openAttribute( "PYTABLES_FORMAT_VERSION" ));
+    // // Attribute * attr = new Attribute (group->openAttribute( "pandas_type" ));
     // Attribute * attr = new Attribute (group->openAttribute( "pandas_type" ));
-    Attribute * attr = new Attribute (group->openAttribute( "pandas_type" ));
-    DataType * type = new DataType (attr->getDataType());
-    attr->read( *type, test );
-    cout << test << endl;
-    delete attr;
-    delete type;
-    delete group;
-    delete file;
+    // DataType * type = new DataType (attr->getDataType());
+    // attr->read( *type, test );
+    // cout << test << endl;
+    // delete attr;
+    // delete type;
+    // delete group;
+    // delete file;
 
     // hid_t file_id;
     // file_id = file.getLocId();
@@ -113,8 +117,8 @@ TwoSigmaFinModTools::TwoSigmaFinModTools(bool true_or_false = false, int n = 11)
         * Get order of datatype and print message if it's a little endian.
         */
         H5std_string * order_string;
-        H5T_order_t * order = new H5T_order_t (intype->getOrder( *order_string ));
-        cout << order_string << endl;
+        // H5T_order_t * order = new H5T_order_t (intype->getOrder( *order_string ));
+        // cout << order_string << endl;
 
         /*
         * Get size of the data element stored in file and print it.
@@ -137,102 +141,142 @@ TwoSigmaFinModTools::TwoSigmaFinModTools(bool true_or_false = false, int n = 11)
     * Get the dimension size of each dimension in the dataspace and
     * display them.
     */
-    // hsize_t dims_out[1];
-    hsize_t (*dims_out)[1];
-    int * ndims = new int (dataspace->getSimpleExtentDims( *dims_out, NULL));
+    // hsize_t (*dims_out) [1];
+    hsize_t * dims_out = new hsize_t [2];
+    int * ndims = new int (dataspace->getSimpleExtentDims( dims_out, NULL));
     cout << "ndims " << *ndims << endl;
     cout << "rank " << *rank << ", dimensions " <<
-        (unsigned long)(dims_out[0]) << endl;
-        // (unsigned long)(dims_out[0]) << " x " <<
-        // (unsigned long)(dims_out[1]) << endl;
+        // (unsigned long)(dims_out[0]) << endl;
+        (unsigned long)(dims_out[0]) << " x " <<
+        (unsigned long)(dims_out[1]) << endl;
     delete rank;
     delete ndims;
     delete dims_out;
     
     
     // hyperslab dimensions
-    const int NX_SUB = 2;
-    const int NY_SUB = 1;
+    const int NX_SUB = 10;
+    const int NY_SUB = 2;
     // const int NX = 1710756;        // output buffer dimensions
-    const int NX = 2;
-    const int NY = 1;
+    const int NX = NX_SUB;
+    const int NY = NY_SUB;
+    // const int NX = 10;
+    // const int NY = 2;
     // const int NX = 18000; 
     // const int NY = 111;
-    const int RANK_OUT = 1;
+    const int RANK_OUT = 2;
     /* 
      * Define hyperslab in dataset. Implicitly with strike and 
      * block NULL.      
      */
-    hsize_t offset[1];
-    hsize_t count[1];
+    hsize_t offset[2];
+    hsize_t count[2];
     offset[0] = 0;
-    // offset[1] = 0;
+    offset[1] = 0;
     count[0] = NX_SUB;
-    // count[1] = NY_SUB;
+    count[1] = NY_SUB;
     dataspace->selectHyperslab( H5S_SELECT_SET, count , offset );
     /* 
      * Define memory dataspace.
      */
-    hsize_t dimsm[1];
+    hsize_t dimsm[2];
     dimsm[0] = NX;
-    // dimsm[1] = NY;
+    dimsm[1] = NY;
     DataSpace * memspace = new DataSpace ( RANK_OUT, dimsm );
 
     /* 
      * Define memory hyperslab.
      */
-    hsize_t offset_out[1];
-    hsize_t count_out[1];
+    hsize_t offset_out[2];
+    hsize_t count_out[2];
     offset_out[0] = 0;
-    // offset_out[1] = 0;
+    offset_out[1] = 0;
     count_out[0] = NX_SUB;
-    // count_out[1] = NY_SUB;
+    count_out[1] = NY_SUB;
     memspace->selectHyperslab( H5S_SELECT_SET, count_out, offset_out );
 
     /* 
      * Read data from file's hyperslab into the hyperslab of memory and display data.
      */
     int i, j;
-    // int data_out[NX][NY];  /* Output buffer */
-    H5std_string * data_out = new H5std_string [NX];  /* Output buffer */
+    
+    // int ** data_out = new int * [NX];  /* Output buffer */
+    // for (i = 0; i < NX; i++)
+    // {
+    //     data_out[i] = new int [NY];
+    // }
+
+    // Define two dim array in static memory. Memory is cleared after leaving scope
+    int data_out[NX][NY];  /* Output buffer */    
+    
+    // int data_out[NX];  /* Output buffer */
+    // H5std_string * data_out = new H5std_string [NX];  /* Output buffer */
+    // std::string * data_out = new std::string [NX];  /* Output buffer */
+    // H5T_class_t * data_out = new H5T_class_t [NX];
+    // H5T_STRING * data_out = new H5T_STRING [NX];  /* Output buffer */
+    // string * data_out = new string [NX];  /* Output buffer */
     // int data_out[NX];  /* Output buffer */
     // H5T_STRING data_out[NX];  /* Output buffer */
     // PredType::C_S1 data_out[NX][NY];  /* Output buffer */
-    // for (i = 0; i < NX; i++)
-    // {
-    //     for (j = 0; j < NY; j++)
-    //     {
-    //         // data_out[i][j] = 0;
-    //         data_out[i][j] = "0";
-    //         // data_out[i][j] = '*';
-    //     }
-    // }
 
-    for (i = 0; i < NX; i++)
+    if ( RANK_OUT == 2 )
     {
-        data_out[i] = "0";
-        // data_out[i] = 0;
+        // int two_dim_ar[NX][NY];  /* Output buffer */
+        // int * data_out = two_dim_ar;
+        for (i = 0; i < NX; i++)
+        {
+            for (j = 0; j < NY; j++)
+            {
+                // two_dim_ar[i][j] = 0;
+                data_out[i][j] = 0;
+                // (*data_out[i])[j] = 0;
+                // data_out[i][j] = "0";
+                // data_out[i][j] = '*';
+            }
+        }
     }
-        
-    dataset->read( data_out, PredType::C_S1, *memspace, *dataspace );
-    // dataset->read( data_out , PredType::C_S1 );
+    else
+    {
+        int * data_out = new int [NX*NY];  /* Output buffer */
+        for (i = 0; i < NX*NY; i++)
+        {
+            // data_out[i] = "0";
+            data_out[i] = 0;
+        }
+    }
+
+    // dataset->read( data_out, PredType::NATIVE_INT );
+    dataset->read( data_out, PredType::NATIVE_INT, *memspace, *dataspace );
     // for (i = 0; i < NX; i++)
     // {
-    //     for (j = 0; j < NY; j++)
-    //     {
-    //         cout << data_out[i][j] << " ";
-    //     }
-    //     cout << endl;
+    //     dataset->read( data_out, PredType::NATIVE_INT, *memspace, *dataspace );
     // }
-    for (i = 0; i < NX; i++)
+    // dataset->read( data_out, PredType::C_S1, *memspace, *dataspace );
+    // dataset->read( data_out , PredType::C_S1 );
+    if (RANK_OUT == 2)
     {
-        cout << data_out[i];
+        for (i = 0; i < NX; i++)
+        {
+            for (j = 0; j < NY; j++)
+            {
+                cout << data_out[i][j] << " ";
+            }
         cout << endl;
+        }
+
+    }
+    else
+    {
+        for (i = 0; i < NX; i++)
+        {
+            cout << data_out[2*i] << " " << data_out[2*i + 1];
+            cout << endl;
+       }
     }
     delete dataset;
     delete memspace;
     delete dataspace;
-    // delete data_out;
+    // delete [] data_out;
 }
 
 // destructor
